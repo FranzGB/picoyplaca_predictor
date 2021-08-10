7d85406b83de7402227c0c92876183d1e1b40aef
Franz Guzmán <46214532+FranzGB@users.noreply.github.com>
2021-08-10 17:43:40 -0500

    Add main function

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import sys
+
 class Car:
     '''This is a car class'''   
     def __init__(self,var):
         self.license_plate = var
-
+    
+    
 #Create a new object of Date class
-rojo = Car("PBY-0168")
+#rojo = Car("PBY-0168")
 
 class Date:
     '''This is a date class. It holds the date in format MM/DD/YYYY and hour'''
-    def __init__(self):
-        self.date = '01/01/1999'
-        self.hour = '00:00'
+    def __init__(self,d,h):
+        self.date = d
+        self.hour = h
 
 #Create a new object of Date class
-current_date = Date()
+#current_date = Date('01/01/1111','00:00')
+
+def main():
+    mycar = Car(sys.argv[1])
+    mydate = Date(sys.argv[2],sys.argv[3])
+   
+main()
